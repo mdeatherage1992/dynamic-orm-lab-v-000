c78c9e1495efa937c882909b26c9fa1aad01e518
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-06 17:19:52 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -27,6 +27,10 @@ def initialize(options={})
     end
   end
 
+  self.column_names.each do |col_name|
+   attr_accessor col_name.to_sym
+ end
+
 
 def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
