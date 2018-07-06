50642feb8c13c62dee5ccc51fba9154f0a957bfa
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-06 17:48:58 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -27,9 +27,9 @@ def initialize(options={})
   end
 
 
-  self.column_names.each do |col_name|
-     attr_accessor col_name.to_sym
-   end
+  # self.column_names.each do |col_name|
+  #    attr_accessor col_name.to_sym
+  #  end
 
 
 
